15cbe78cf626597c48d7d6f7a7e94a52261e32dc
lilingfeng <2632721064@qq.com>
2021-10-07 18:30:45 +0800

    third commit

--- src/com/feng/test1/practice.java
+++ src/com/feng/test1/practice.java
@@ -3,6 +3,7 @@ package com.feng.test1;
 public class practice {
     public static void main(String[] args) {
         int index;
+        int index2;
         int sum=0;
         for (int i=1;i<=2;i++){
             for (int n=1;n<=i;n++){
